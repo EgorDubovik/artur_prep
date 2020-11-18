1c6cb4fcd8560c144c1f8c5539b899c18647fed8
EgorDubovik <posik.web.m@gmail.com>
2020-11-18 08:27:55 -0600

    view errors

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
 	include 'language/RU.php';
 	include 'language/EN.php';
 	$arrLang = ['ru'=>$_RU,'en'=>$_EN];
