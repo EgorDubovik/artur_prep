93d4b79001c5214aa8ae259548d5af634b7b125e
EgorDubovik <posik.web.m@gmail.com>
2020-11-23 19:35:14 -0600

    fixes

--- mail.php
+++ mail.php
@@ -16,7 +16,7 @@ if(isset($_POST['event']))
 try {
     //Recipients
     $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('posik.web.m@gmail.com', 'Joe User');     // Add a recipient
+    $mail->addAddress('posik.web.m@gmail.com');     // Add a recipient
     $mail->addReplyTo('info@example.com', 'Information');
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
